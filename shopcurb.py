45a7be01c5fbef4c14df774fd544a484d0010a05
lemongod <wang.q.vincent@gmail.com>
2015-07-20 23:57:19 -0700

    Fixed, parses malformed JSON keys correctly now

--- shopcurb.py
+++ shopcurb.py
@@ -1,4 +1,5 @@
 #Vincent Wang
+#wang.q.vincent@gmail.com
 import requests
 import json
 
@@ -16,22 +17,27 @@ counter = 0
 #basic DFS
 while stack:
     counter += 1
-    if counter >= 10:
+    if counter >= 10: #must refresh header key
         key = requests.get(keyurl)
         head = {'Session': key.text}
         counter = 0
     vertex = stack.pop()
     r = requests.get(baseurl+vertex, headers=head)
-    parsed_json = json.loads(r.text)
+
+    parsed_json = json.loads(r.text) #malformed JSON keys fix
+    for key in parsed_json.keys():
+        parsed_json[key.lower()] = parsed_json[key]
+
     if parsed_json.get('next'):
         next_nodes = parsed_json['next']
-        if vertex not in visited:
+        if vertex not in visited: #not actually necessary since secrets are only on lowest depth
             visited.add(vertex)
-            if type(next_nodes) == list:
+            if type(next_nodes) == list: #if it's a list of nodes
                 for next_vertex in next_nodes:
                     stack.append(next_vertex)
             else:
-                stack.append(next_nodes)
+                stack.append(next_nodes) #entire list is just one node
     if parsed_json.get('secret'):
         ans = ans + parsed_json['secret']
+
 print ans[::-1] #python! string reversal
